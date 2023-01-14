eb00480af2c87297f89323795770f89c271cdd6a
bigz4 <bzapf6@gmail.com>
2023-01-13 21:14:03 -0800

    Update groups.php

--- src/app/Http/Controllers/groups.php
+++ src/app/Http/Controllers/groups.php
@@ -5,6 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+//this controller handles the routes and transfer of data for the groups page
+
+//FUNCTION DESCRIPTIONS:
+
+//getBookedGroups gets all groups that have been booked
+//editBookedGroups brings the user to the edit bookings screen and displays all data in the data base for the given id
+//updateBookedGroups updates the data entered on the edit
+
+
 class groups extends Controller
 {
     //TODO: for all change variables as needed for database and frontend connection
@@ -20,6 +29,7 @@ class groups extends Controller
     }
 
     //TODO:
+
     //brings the user to the edit bookings screen and displays all data in the data base for the given id
     function editBookedGroups($id){
         $data = DB::table('')->find($id);
@@ -27,6 +37,7 @@ class groups extends Controller
     }
 
     //TODO:
+    
     //updates the data entered on the edit 
     function updateBookedGroups(Request $req){
         DB::table('')->where('id', $req->id)->update([
@@ -49,6 +60,7 @@ class groups extends Controller
     }
 
     //TODO:
+    
     function assignAccomidation(Request $req){
 
     }

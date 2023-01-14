7604c167817d6fa715c062bd46b325078a7e67f7
bigz4 <bzapf6@gmail.com>
2023-01-13 21:15:03 -0800

    Update regionsController.php

--- src/app/Http/Controllers/regionsController.php
+++ src/app/Http/Controllers/regionsController.php
@@ -5,10 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+//this controller handles the routes and transfer of data for the groups page
+
+//FUNCTION DESCRIPTIONS:
+
+//getRegions gets all groups that have been booked
+//editRegions brings the user to the edit bookings screen and displays all data in the data base for the given id
+//updateRegions updates the data entered on the edit
+//addRegions adds a regions into the database
+
 class regionsController extends Controller
 {
     //TODO: for all change variables as needed for database and frontend connection
 
+    //gets all groups that have been booked
     function getRegions(){
         $result = DB::table('')->select('name','totalAccom','numBookings', 'description')->get();
 
@@ -16,11 +26,14 @@ class regionsController extends Controller
     }
    
     //TODO:
+
+    //brings the user to the edit bookings screen and displays all data in the data base for the given id
     function editRegions($id){
         $data = DB::table('')->find($id);
         return view("",['data'=>$data]);
     }
 
+    //updates the data entered on the edit
     function updateReqions(Request $req){
         DB::table('')->where('id', $req->id)->update([
             'name' => $req->name,
@@ -31,7 +44,9 @@ class regionsController extends Controller
 
         return redirect("");
     }
+
     //TODO:
+    //adds a regions into the database
     function addRegions(Request $reg){
         
         DB::table('')->insert([

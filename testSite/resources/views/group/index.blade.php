703f9a64fccfec22274cdbc1528054fee6b4c2ad
bigz4 <bzapf6@gmail.com>
2023-01-27 22:21:16 -0800

    added search to groups

--- testSite/resources/views/group/index.blade.php
+++ testSite/resources/views/group/index.blade.php
@@ -1,4 +1,24 @@
 <x-app-layout>
+<head>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
+          <!-- Javascript -->
+        <script>
+            $(function() {
+                var availableTutorials  =  [
+                "ActionScript",
+                "Bootstrap",
+                "C",
+                "C++",
+                ];
+                $( "#automplete-1" ).autocomplete({
+                source: availableTutorials
+                });
+            });
+        </script>
+    </head>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         <form method="POST" action="{{ route('group.store') }}">
             @csrf

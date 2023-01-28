fe10007c195870ab661fa30c44debfabba2d18cd
mashadchowdhury <78212071+mashadchowdhury@users.noreply.github.com>
2023-01-27 23:56:09 -0800

    update

--- testSite/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ testSite/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -1,5 +1,5 @@
 <?php
-
+//pass
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
--- testSite/routes/channels.php
+++ testSite/routes/channels.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+//h
 use Illuminate\Support\Facades\Broadcast;
 
 /*

a9f1a217670eb70e053a017183f2cfb1270f15b1
bigz4 <bzapf6@gmail.com>
2023-03-14 20:01:23 -0700

    added logic to update regions table on booking

--- testSite/app/Http/Controllers/BookingController.php
+++ testSite/app/Http/Controllers/BookingController.php
@@ -47,11 +47,21 @@ class BookingController extends Controller
             'from_date' => 'required|dateTime',
             'to_date' => 'required|dateTime',
         ]); */
-        $accom_num = Accommodation::select('number_of_beds')->where('name_accommodation', '=', $validated['name_accommodation'])->get();
+        $accom_num = Accommodation::select('number_of_beds','name_region')->where('name_accommodation', '=', $validated['name_accommodation'])->get();
         $group_size = Group::select('group_count')->where('name_group', '=', $validated['name_group'])->get();
+        $region_name = $accom_num->value('name_region');
         $accom_num = $accom_num->value('number_of_beds');
         $group_size = $group_size->value('group_count');
         
+        $num_bookings = Region::select('amount_bookings','amount_accommodations')->where('name_region', '=', $region_name)->get();
+        $region_accomm = $num_bookings->value("amount_accommodations");
+        $num_bookings = $num_bookings->value('amount_bookings');
+
+        if($accom_num == $group_size){
+            Region::where('name_region', '=', $region_name)->update(["amount_accommodations" => $region_accomm - 1]);
+        }
+        
+        Region::where('name_region', '=', $region_name)->update(["amount_bookings" => $num_bookings + 1]);
         Accommodation::where('name_accommodation', '=', $validated['name_accommodation'])->update(['number_of_beds' => $accom_num - $group_size]);
         $request->user()->bookings()->create($validated);
         return redirect(route('booking.index'));
@@ -111,11 +121,21 @@ class BookingController extends Controller
      */
     public function destroy(Booking $booking)
     {
-        $accom_num = Accommodation::select('number_of_beds')->where('name_accommodation', '=', $booking->name_accommodation)->get();
+        $accom_num = Accommodation::select('number_of_beds','name_region')->where('name_accommodation', '=', $booking->name_accommodation)->get();
         $group_size = Group::select('group_count')->where('name_group', '=', $booking->name_group)->get();
+        $region_name = $accom_num->value('name_region');
         $accom_num = $accom_num->value('number_of_beds');
         $group_size = $group_size->value('group_count');
+
+        $num_bookings = Region::select('amount_bookings','amount_accommodations')->where('name_region', '=', $region_name)->get();
+        $region_accomm = $num_bookings->value("amount_accommodations");
+        $num_bookings = $num_bookings->value('amount_bookings');
+
+        if($accom_num == 0){
+            Region::where('name_region', '=', $region_name)->update(["amount_accommodations" => $region_accomm + 1]);
+        }
         
+        Region::where('name_region', '=', $region_name)->update(["amount_bookings" => $num_bookings - 1]);
         Accommodation::where('name_accommodation', '=', $booking->name_accommodation)->update(['number_of_beds' => $accom_num + $group_size]);
         $this->authorize('delete', $booking);
         $booking->delete();

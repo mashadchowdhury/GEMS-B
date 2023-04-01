c93d1757b0c5ed6f326a7d27da481e2d738fc30a
mashadchowdhury <78212071+mashadchowdhury@users.noreply.github.com>
2023-03-31 23:47:21 -0700

    clean up

--- testSite/resources/views/auth/verify-email.blade.php
+++ testSite/resources/views/auth/verify-email.blade.php
@@ -2,7 +2,6 @@
     <div class="mb-4 text-sm text-gray-600">
         {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
     </div>
-
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600">
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}
